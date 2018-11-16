65b07b56bb7c273eba2102c9457d6fff5fe9943d
Sirak Berhane <sirakberhane2979@gmail.com>
2018-11-16 18:28:55 -0500

    New Package for Unit Test

---
+++ Plants vs Zombies/src/test/PlantTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import plant.Plant;
+
+public class PlantTest {
+
+private Plant pl;
+	
+	public void setUp() throws Exception{
+		pl = new Plant(5,50,1,1);
+	}
+	
+	public void testPlant(){
+		assertNotNull(pl);
+	}
+	public void testPlantCost(){
+		assertEquals(10,pl.getBuyThreshold());
+	}
+	public void testPlantHit(){
+		assertEquals(3,pl.getHitThreshold());
+	}
+	public void setTestPlantHit(){
+		pl.setHitThreshold(5);
+		assertEquals(5,pl.getHitThreshold());
+	}
+	public void testXPos(){
+		assertEquals(1,pl.getxPos());
+	}
+	public void testYPos(){
+		assertEquals(1,pl.getyPos());
+	}
+}
+
