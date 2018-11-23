3a3d58eba8c5f4582a79cd5c4f87a79df916085b
Sirak Berhane <sirakberhane2979@gmail.com>
2018-11-23 13:28:33 -0500

    Added more removed code

--- Plants vs Zombies/src/plant/ExplosivePlant.java
+++ Plants vs Zombies/src/plant/ExplosivePlant.java
@@ -1,5 +1,9 @@
 package plant;
 
-public class ExplosivePlant {
+public class ExplosivePlant extends Plant{
+
+	public ExplosivePlant(int hitThreshold, int buyThreshold, int x, int y) {
+		super(hitThreshold, buyThreshold, x, y);
+	}
 
 }
