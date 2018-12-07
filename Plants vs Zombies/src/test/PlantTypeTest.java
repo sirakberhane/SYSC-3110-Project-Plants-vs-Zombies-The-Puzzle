b8b1595b347739709d4b2f4625f7933907902fff
Sirak Berhane <sirakberhane2979@gmail.com>
2018-12-07 16:38:57 -0500

    Plant test cases added

--- Plants vs Zombies/src/test/PlantTypeTest.java
+++ Plants vs Zombies/src/test/PlantTypeTest.java
@@ -20,18 +20,38 @@ public class PlantTypeTest extends TestCase{
 	}
 
 	public void testHypnoShroomPlantType() {
+		// Check that the super class is initializing everything
+		assertEquals(0, hs.getxPos());
+		assertEquals(0, hs.getyPos());
 
+		// Check that the plant is still alive
+		assertFalse(hs.isPlantDead());
 	}
 
 	public void testPotatoMinePlantType() {
+		// Check that the super class is initializing everything
+		assertEquals(0, pm.getxPos());
+		assertEquals(1, pm.getyPos());
 
+		// Check that the plant is still alive
+		assertFalse(pm.isPlantDead());
 	}
 
 	public void testSnowPeashooterPlantType() {
+		// Check that the super class is initializing everything
+		assertEquals(0, sps.getxPos());
+		assertEquals(2, sps.getyPos());
 
+		// Check that the plant is still alive
+		assertFalse(sps.isPlantDead());
 	}
 
 	public void testWallnutPlantType() {
+		// Check that the super class is initializing everything
+		assertEquals(0, wn.getxPos());
+		assertEquals(3, wn.getyPos());
 
+		// Check that the plant is still alive
+		assertFalse(wn.isPlantDead());
 	}
 }
