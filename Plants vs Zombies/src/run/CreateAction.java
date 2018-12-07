ea0de26129e4deeea3f3b7c52b25b0488d1345cb
jolartabungar <35468434+jolartabungar@users.noreply.github.com>
2018-12-07 18:52:33 -0500

    Added Level Building Feature
    Can now create your own levels:
    - Select how many waves
    - Select how many zombies per wave

--- Plants vs Zombies/src/run/CreateAction.java
+++ Plants vs Zombies/src/run/CreateAction.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class CreateAction implements MouseListener {
-	//Reference to the GameGUI
+	// Reference to the GameGUI
 	private GameGUI game;
 	private ArrayList<Integer> waveSizes;
 	private WriteFile writeFile;
 
-	//Create a new PeashooterSelectController with the reference to the GameGUI
+	// Create a new PeashooterSelectController with the reference to the GameGUI
 	public CreateAction(GameGUI game) {
 		this.game = game;
 		waveSizes = new ArrayList<Integer>();
@@ -21,19 +21,87 @@ public class CreateAction implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		int numWaves = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of waves"));
-		
-		int i = 0;
-		while (i < numWaves) {
-			int numZombies = Integer.parseInt(JOptionPane.showInputDialog(
-					"Enter the number of zombies for Wave " + (i + 1)));
-			i ++;
-			waveSizes.add(numZombies);
+		String result = null;
+		boolean isNum = false;
+		boolean cancelCreation = false;
+		while (!isNum) {
+			try {
+				result = null;
+				result = JOptionPane.showInputDialog("Enter the number of waves");
+
+				if (result != null) {
+					// checking valid integer using parseInt() method
+					Integer test = Integer.parseInt(result);
+					if (test > 1)
+						isNum = true;
+				}
+
+				else {
+					cancelCreation = true;
+					break;
+				}
+			} catch (NumberFormatException c) {
+				isNum = false;
+			}
+
+			System.out.println(result);
 		}
-		
-		String levelName = JOptionPane.showInputDialog("Enter the name of the level: ");
-		
-		writeFile.writeXmlFile(waveSizes, levelName);
+
+		Integer numWaves = null;
+		if (result != null)
+			numWaves = Integer.parseInt(result);
+
+		if (numWaves != null) {
+			int i = 0;
+			while (i < numWaves) {
+
+				isNum = false;
+				String resultNumZombie = null;
+				while (!isNum) {
+					
+					try {
+						resultNumZombie = null;
+						resultNumZombie = JOptionPane
+								.showInputDialog("Enter the number of zombies for Wave " + (i + 1));
+
+						if (resultNumZombie != null) {
+							Integer test = Integer.parseInt(resultNumZombie);
+							if (test > 1)
+								isNum = true;
+						}
+						else {
+							cancelCreation = true;
+							break;
+						}
+
+					} catch (NumberFormatException c) {
+						isNum = false;
+					}
+
+				}
+
+				if (resultNumZombie != null) {
+					Integer numZombies = Integer.parseInt(resultNumZombie);
+					i++;
+					if (numZombies != null)
+						waveSizes.add(numZombies);
+
+				}
+				else {
+					cancelCreation = true;
+					break;
+				}
+
+			}
+
+			if (!cancelCreation) {
+				String levelName = JOptionPane.showInputDialog("Enter the name of the level: ");
+
+				if (levelName != null)
+					writeFile.writeXmlFile(waveSizes, levelName);
+			}
+		}
+
 	}
 
 	@Override
